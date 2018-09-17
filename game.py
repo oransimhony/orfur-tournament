0a254c94814a63b72230c1a04e3d56ccf1f67501
Oran Simhony <oran.simhony@gmail.com>
2018-09-17 15:28:53 +0300

    initial commit

---
+++ game.py
@@ -0,0 +1,405 @@
+#1 Import libraries
+import pygame, math, random
+from pygame.locals import *
+import socket
+import threading
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s_host = ("127.0.0.1", 8888)
+pid = 0
+
+p1 = [100, 100, 0.0]
+p2 = [700, 100, 3.0]
+p3 = [100, 500, 0.0]
+p4 = [700, 500, 3.0]
+players = [p1, p2, p3, p4]
+
+
+class receiveThread(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        global p1
+        global p2
+        global p3
+        global p4
+        global playerpos
+
+        print "Listening"
+        while True:
+            (data, addr) = my_socket.recvfrom(1024)
+            print "The server sent: " + data
+            data = data.split(",")
+            code = data[0]
+
+            if code == "zz":
+                p = data[1]
+                x = data[2]
+                y = data[3]
+                angle = data[4]
+                if p == "1":
+                    if pid == p:
+                        players.remove(playerpos)
+                        playerpos = [int(x), int(y), float(angle)]
+                        players.append(playerpos)
+                    else:
+                        players.remove(p1)
+                        p1 = [int(x), int(y), float(angle)]
+                        players.append(p1)
+                elif p == "2":
+                    if pid == p:
+                        players.remove(playerpos)
+                        playerpos = [int(x), int(y), float(angle)]
+                        players.append(playerpos)
+                    else:
+                        players.remove(p2)
+                        p2 = [int(x), int(y), float(angle)]
+                        players.append(p2)
+                elif p == "3":
+                    if pid == p:
+                        players.remove(playerpos)
+                        playerpos = [int(x), int(y), float(angle)]
+                        players.append(playerpos)
+                    else:
+                        players.remove(p3)
+                        p3 = [int(x), int(y), float(angle)]
+                        players.append(p3)
+                elif p == "4":
+                    if pid == p:
+                        players.remove(playerpos)
+                        playerpos = [int(x), int(y), float(angle)]
+                        players.append(playerpos)
+                    else:
+                        players.remove(p4)
+                        p4 = [int(x), int(y), float(angle)]
+                        players.append(p4)            # get_player_pos()
+
+def initialConnect():
+    pass
+#
+# class sendThread(threading.Thread):
+#     def __init__(self, name):
+#         threading.Thread.__init__(self)
+#         self.name = name
+#         initialConnect()
+#     def run(self):
+#         print "Starting connection"
+#         connection()
+#         print "Ending connection"
+
+# connectionThread = sendThread("1")
+lThread = receiveThread("1")
+
+
+def initialConnect():
+    global pid
+    global lThread
+
+    my_socket.sendto("00", s_host)
+    (data, addr) = my_socket.recvfrom(1024)
+    print "The server sent: " + data
+    try:
+        pid = data.split("#")[1]
+        lThread.start()
+        # lThread.run()
+    except Exception as e:
+        print e.message
+        print "You can't connect the lobby right now"
+        disconnect()
+        my_socket.close()
+        pygame.quit()
+        exit(0)
+
+
+
+# def connection():
+#     get_player_pos()
+
+def send_player_pos():
+    s = ""
+    # Send Player coordinates
+    for playerp in players:
+        if playerp is playerpos:
+            for co in playerp:
+                s += str(co) + ","
+    my_socket.sendto("1" + str(pid) + s[:-1], s_host)
+    # (data, addr) = my_socket.recvfrom(1024)
+    # print "The server sent: " + data
+
+def send_player_keys():
+    s = ""
+    # Send Keys
+    for key in keys:
+        s += str(key) + ","
+    my_socket.sendto("2" + str(pid) + s[:-1], s_host)
+
+def get_player_pos():
+    global playerpos
+    global oplayerspos
+    global angle
+    global oplayerangle
+
+    oplayerspos = []
+    oplayerangle = []
+
+    # my_socket.sendto("3" + str(pid), s_host)
+    (data, addr) = my_socket.recvfrom(1024)
+    #print "The server sent: " + data
+    pos = data.split(',')
+    playerpos = [int(pos[0]), int(pos[1]), float(pos[2])]
+
+    if pid == "1":
+        my_socket.sendto("32", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("33", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("34", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+    elif pid == "2":
+        my_socket.sendto("31", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("33", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("34", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+    elif pid == "3":
+        my_socket.sendto("31", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("32", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("34", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+    elif pid == "4":
+        my_socket.sendto("31", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("32", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        oplayerangle.append(float(pos[2]))
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+        my_socket.sendto("33", s_host)
+        (data, addr) = my_socket.recvfrom(1024)
+        #print "The server sent: " + data
+        pos = data.split(',')
+        oplayerspos.append([int(pos[0]), int(pos[1])])
+        oplayerangle.append(float(pos[2]))
+
+def disconnect():
+    my_socket.sendto("99", s_host)
+    (data, addr) = my_socket.recvfrom(1024)
+    print "The server sent: " + data
+
+
+
+#2 Initialize the game
+pygame.init()
+pygame.mixer.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Top Down Shooter")
+flags = screen.get_flags()
+keys = [False, False, False, False]
+arrow_speed = 40
+player_speed = 6
+clock = pygame.time.Clock()
+frame = 0
+
+#3 Load images
+player = pygame.image.load('player.png') # 62 41
+player = pygame.transform.scale(player, (62, 41))
+arrow = pygame.image.load('bullet1.png')
+arrow = pygame.transform.scale(arrow, (14, 6))
+background = pygame.image.load('bg1.jpg')
+background = pygame.transform.scale(background, (width, height))
+
+
+# connectionThread.start()
+initialConnect()
+
+if pid == "1":
+    playerpos = [p1[0], p1[1], p1[2]]
+    players.remove(p1)
+    players.append(playerpos)
+elif pid == "2":
+    playerpos = [p2[0], p2[1], p2[2]]
+    players.remove(p2)
+    players.append(playerpos)
+elif pid == "3":
+    playerpos = [p3[0], p3[1], p3[2]]
+    players.remove(p3)
+    players.append(playerpos)
+elif pid == "4":
+    playerpos = [p4[0], p4[1], p4[2]]
+    players.remove(p4)
+    players.append(playerpos)
+
+oplayerspos = []
+oplayerangle = []
+arrows = []
+
+#4 Keep looping through
+running = 1
+exitcode = 0
+while running:
+    #5 Clear the screen before drawing it agains
+    screen.fill((255, 255, 255))
+    screen.blit(background, (0, 0))
+    #6.1 Set player position and rotation
+    # position = pygame.mouse.get_pos()
+    # playerpos[2] = math.atan2(position[1] - (playerpos[1] + 31), position[0] - (playerpos[0] + 20))
+    # playerrot = pygame.transform.rotate(player, 360-playerpos[2] * 57.29)
+    # playerpos1 = (playerpos[0] - playerrot.get_rect().width / 2, playerpos[1] - playerrot.get_rect().height / 2)
+    # screen.blit(playerrot, playerpos1)
+    #
+    # # for pos in oplayerspos:
+    # #     oplayerrot = pygame.transform.rotate(player, 360-oplayerangle[i] * 57.29)
+    # #     oplayerpos1 = (pos[0] - oplayerrot.get_rect().width / 2, pos[1] - oplayerrot.get_rect().height / 2)
+    # #     screen.blit(oplayerrot, oplayerpos1)
+    # #     i += 1
+    # playerrot = pygame.transform.rotate(player, 360 - p2[2] * 57.29)
+    # playerpos1 = (p2[0] - playerrot.get_rect().width / 2, p2[1] - playerrot.get_rect().height / 2)
+    # screen.blit(playerrot, playerpos1)
+    #
+    # playerrot = pygame.transform.rotate(player, 360 - p3[2] * 57.29)
+    # playerpos1 = (p3[0] - playerrot.get_rect().width / 2, p3[1] - playerrot.get_rect().height / 2)
+    # screen.blit(playerrot, playerpos1)
+    #
+    # playerrot = pygame.transform.rotate(player, 360 - p4[2] * 57.29)
+    # playerpos1 = (p4[0] - playerrot.get_rect().width / 2, p4[1] - playerrot.get_rect().height / 2)
+    # screen.blit(playerrot, playerpos1)
+
+    for playerp in players:
+        if playerp is playerpos:
+            position = pygame.mouse.get_pos()
+            playerp[2] = math.atan2(position[1] - (playerp[1] + 31), position[0] - (playerp[0] + 20))
+        playerrot = pygame.transform.rotate(player, 360 - playerp[2] * 57.29)
+        playerpos1 = (playerp[0] - playerrot.get_rect().width / 2, playerp[1] - playerrot.get_rect().height / 2)
+        screen.blit(playerrot, playerpos1)
+
+    for bullet in arrows:
+        index = 0
+        velx = math.cos(bullet[0]) * arrow_speed
+        vely = math.sin(bullet[0]) * arrow_speed
+        bullet[1] += velx
+        bullet[2] += vely
+        if bullet[1] < -64 or bullet[1] > width or bullet[2] < -64 or bullet[2] > height:
+            arrows.pop(index)
+        index += 1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
+
+    #7 Update the screen
+    pygame.display.flip()
+
+    #8 Loop through the events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            disconnect()
+            my_socket.close()
+            pygame.quit()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_w:
+                keys[0] = True
+            elif event.key == K_a:
+                keys[1] = True
+            elif event.key == K_s:
+                keys[2] = True
+            elif event.key == K_d:
+                keys[3] = True
+            elif event.key == K_f:
+                # toggle fullscreen by pressing F key.
+                if flags & FULLSCREEN == False:
+                    flags |= FULLSCREEN
+                    pygame.display.set_mode((width, height), flags)
+        if event.type == pygame.KEYUP:
+            if event.key == K_w:
+                keys[0] = False
+            elif event.key == K_a:
+                keys[1] = False
+            elif event.key == K_s:
+                keys[2] = False
+            elif event.key == K_d:
+                keys[3] = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            arrows.append([math.atan2(position[1] - (playerpos[1] + 31), position[0] - (playerpos[0] + 20)), playerpos[0] + 32, playerpos1[1] + 32])
+    #9 Move player
+    if keys[0]:
+        playerpos[1] -= player_speed
+        send_player_pos()
+    elif keys[2]:
+        playerpos[1] += player_speed
+        send_player_pos()
+    if keys[1]:
+        playerpos[0] -= player_speed
+        send_player_pos()
+    elif keys[3]:
+        playerpos[0] += player_speed
+        send_player_pos()
+
+    clock.tick(30)
+    # frame += 1
+    # if frame == 15:
+    #     print p1
+    #     print p2
+    #     print p3
+    #     print p4
+    #     print playerpos
+    #     frame = 0
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            disconnect()
+            my_socket.close()
+            pygame.quit()
+            exit(0)
+    pygame.display.flip()
---
+++ server.py
@@ -0,0 +1,169 @@
+import socket
+import select
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+host = ""
+port = 8888
+
+p1 = [100, 100, 0.0]
+p2 = [700, 100, 3.0]
+p3 = [100, 500, 0.0]
+p4 = [700, 500, 3.0]
+
+bullet = [player, angle, x, y]
+
+keys = [False, False, False, False]
+
+server_socket.bind((host, port))
+print "Server bounded."
+
+players = {"1": "", "2": "", "3": "", "4": ""}
+addrs = []
+
+while True:
+    try:
+        msg, addr = server_socket.recvfrom(2048)
+        code = msg[:2]
+        print "start - ", msg, code, addr
+        if code == "00":
+            if addr not in addrs:
+                if players["1"] == "":
+                    players["1"] = addr
+                    addrs.append(addr)
+                    server_socket.sendto("You are player #1", addr)
+                    server_socket.sendto("zz,1," + str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+                    server_socket.sendto("zz,2," + str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+                    server_socket.sendto("zz,3," + str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+                    server_socket.sendto("zz,4," + str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+                    for addr in addrs:
+                        server_socket.sendto("T,Player #1 Connected.", addr)
+                elif players["2"] == "":
+                    players["2"] = addr
+                    addrs.append(addr)
+                    server_socket.sendto("You are player #2", addr)
+                    server_socket.sendto("zz,1," + str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+                    server_socket.sendto("zz,2," + str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+                    server_socket.sendto("zz,3," + str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+                    server_socket.sendto("zz,4," + str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+                    for addr in addrs:
+                        server_socket.sendto("T,Player #2 Connected.", addr)
+                elif players["3"] == "":
+                    players["3"] = addr
+                    addrs.append(addr)
+                    server_socket.sendto("You are player #3", addr)
+                    server_socket.sendto("zz,1," + str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+                    server_socket.sendto("zz,2," + str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+                    server_socket.sendto("zz,3," + str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+                    server_socket.sendto("zz,4," + str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+                    for addr in addrs:
+                        server_socket.sendto("T,Player #3 Connected.", addr)
+                elif players["4"] == "":
+                    players["4"] = addr
+                    addrs.append(addr)
+                    server_socket.sendto("You are player #4", addr)
+                    server_socket.sendto("zz,1," + str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+                    server_socket.sendto("zz,2," + str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+                    server_socket.sendto("zz,3," + str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+                    server_socket.sendto("zz,4," + str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+                    for addr in addrs:
+                        server_socket.sendto("T,Player #4 Connected.", addr)
+                else:
+                    server_socket.sendto("Maximum number of players connected.", addr)
+        elif code == "11":
+            print 'Getting pos#1'
+            data = msg[2:]
+            print data, addr
+            data = data.split(',')
+            for i in xrange(len(data) - 1):
+                p1[i] = int(data[i])
+            p1[2] = data[2]
+            server_socket.sendto("OK. " + "\tP1: " + str(p1), addr)
+            # notify all players
+            for addr in addrs:
+                server_socket.sendto("zz,1," + str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+        elif code == "12":
+            print 'Getting pos#2'
+            data = msg[2:]
+            print data, addr
+            data = data.split(',')
+            for i in xrange(len(data) - 1):
+                p2[i] = int(data[i])
+            p2[2] = data[2]
+            server_socket.sendto("OK. " + "\tP2: " + str(p2), addr)
+            # notify all players
+            for addr in addrs:
+                server_socket.sendto("zz,2," + str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+        elif code == "13":
+            print 'Getting pos#3'
+            data = msg[2:]
+            print data, addr
+            data = data.split(',')
+            for i in xrange(len(data) - 1):
+                p3[i] = int(data[i])
+            p3[2] = data[2]
+            server_socket.sendto("OK. " + "\tP3: " + str(p3), addr)
+            # notify all players
+            for addr in addrs:
+                server_socket.sendto("zz,3," + str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+        elif code == "14":
+            print 'Getting pos#4'
+            data = msg[2:]
+            print data, addr
+            data = data.split(',')
+            for i in xrange(len(data) - 1):
+                p4[i] = int(data[i])
+            p4[2] = data[2]
+            server_socket.sendto("OK. " + "\tP4: " + str(p4), addr)
+            # notify all players
+            for addr in addrs:
+                server_socket.sendto("zz,4," + str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+        elif code == "20":
+            print 'Getting keys'
+            data = msg[2:]
+            print data, addr
+            data = data.split(',')
+            for i in xrange(len(data)):
+                keys[i] = True if data[i] == "True" else False
+            server_socket.sendto("OK. " + "\tKEYS: " + str(keys), addr)
+
+        elif code == "31":
+            print 'Sending pos#1'
+            server_socket.sendto(str(p1[0]) + "," + str(p1[1]) + "," + str(p1[2]), addr)
+        elif code == "32":
+            print 'Sending pos#2'
+            server_socket.sendto(str(p2[0]) + "," + str(p2[1]) + "," + str(p2[2]), addr)
+        elif code == "33":
+            print 'Sending pos#3'
+            server_socket.sendto(str(p3[0]) + "," + str(p3[1]) + "," + str(p3[2]), addr)
+        elif code == "34":
+            print 'Sending pos#4'
+            server_socket.sendto(str(p4[0]) + "," + str(p4[1]) + "," + str(p4[2]), addr)
+
+        elif code == "99":
+            if players["1"] == addr:
+                players["1"] = ""
+                addrs.remove(addr)
+                server_socket.sendto("Disconnected player #1", addr)
+            elif players["2"] == addr:
+                players["2"] = ""
+                addrs.remove(addr)
+                server_socket.sendto("Disconnected player #2", addr)
+            elif players["3"] == addr:
+                players["3"] = ""
+                addrs.remove(addr)
+                server_socket.sendto("Disconnected player #3", addr)
+            elif players["4"] == addr:
+                players["4"] = ""
+                addrs.remove(addr)
+                server_socket.sendto("Disconnected player #4", addr)
+
+        else:
+            server_socket.sendto("Wrong code. " + code , addr)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print message
+
+
+    # server_socket.sendto("OK. " + "\tP1: " + str(p1) + "\tKEYS: " + str(keys), addr)
